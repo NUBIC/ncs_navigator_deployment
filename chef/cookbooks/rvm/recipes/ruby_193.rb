0febc5dd8d58b4a5d79386a1774432bbd9c4a805
David Yip <yipdw@northwestern.edu>
2012-05-23 18:57:12 -0500

    Customize dependency list for Redhat.

--- chef/cookbooks/rvm/recipes/ruby_193.rb
+++ chef/cookbooks/rvm/recipes/ruby_193.rb
@@ -3,8 +3,17 @@
 # Recipe:: ruby_193
 
 # Install deps as listed by recent revisions of RVM.
-%w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion).each do |pkg|
-  package pkg
+packages = case node[:platform]
+           when 'debian','ubuntu'
+            %w(build-essential openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev libncurses5-dev automake libtool bison subversion)
+           when 'centos','redhat','fedora','scientific'
+            %w(gcc-c++ patch readline readline-devel zlib zlib-devel libyaml-devel libffi openssl-devel make bzip2 autoconf automake libtool bison)
+           end
+
+packages.each do |pkg|
+  package pkg do
+    action :install
+  end
 end
 
 node.default[:rvm][:ruby][:implementation] = 'ruby'
