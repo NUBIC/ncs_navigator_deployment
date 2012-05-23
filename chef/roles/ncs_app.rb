89008ebd79e5b22518faf9b3f27fde104b65c532
David Yip <yipdw@northwestern.edu>
2012-05-23 18:58:03 -0500

    Install Ruby 1.9.3 to app nodes.

--- chef/roles/ncs_app.rb
+++ chef/roles/ncs_app.rb
@@ -4,7 +4,8 @@ description "Application server nodes for the NCS Navigator application suite"
 run_list(
   "role[ncs_common]",
   "recipe[build-essential]",
-  "recipe[postgresql::client]"
+  "recipe[postgresql::client]",
+  "recipe[rvm::ruby_193]"
 )
 
 default_attributes(
