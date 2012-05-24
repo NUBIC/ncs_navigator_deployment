c05c744e255af147f5efa33f523f592681ba087b
David Yip <yipdw@member.fsf.org>
2012-05-24 00:04:49 -0500

    Remove dist_only? bits in the apache2 recipe.
    dist_only? doesn't seem to work under Chef 0.10.  That, or the Chef
    installation I've got isn't configured properly.  Either way, the code
    paths that would be executed with dist_only? evaluating to true aren't
    needed.

--- chef/cookbooks/passenger/recipes/apache2.rb
+++ chef/cookbooks/passenger/recipes/apache2.rb
@@ -7,12 +7,8 @@
 include_recipe "apache2"
 
 if platform?("centos","redhat")
-  if dist_only?
-    # just the gem, we'll install the apache module within apache2
-    package "rubygem-passenger"
-    return
-  else
-    package "httpd-devel"
+  %w(httpd-devel curl-devel).each do |pkg|
+    package pkg
   end
 else
   %w{ apache2-prefork-dev libapr1-dev libcurl4-openssl-dev }.each do |pkg|
@@ -26,33 +22,19 @@ include_recipe "passenger::install"
 
 rvm_exec_prefix = system("test -e /usr/local/rvm") ? "/usr/local/rvm/bin/rvm default exec" : ""
 
-
 bash "install passenger/apache2" do
   user "root"
   code "#{rvm_exec_prefix} passenger-install-apache2-module --auto"
   creates "#{node[:passenger][:module_path]}"
 end
 
-
-
-if platform?("centos","redhat") and dist_only?
-  package "mod_passenger" do
-    notifies :run, resources(:execute => "generate-module-list"), :immediately
-  end
-
-  file "#{node[:apache][:dir]}/conf.d/mod_passenger.conf" do
-    action :delete
-    backup false
-  end
-else
-  template "#{node[:apache][:dir]}/mods-available/passenger.load" do
-    cookbook "passenger"
-    source "apache.load.erb"
-    owner "root"
-    group "root"
-    mode 0644
-    only_if do ::File.exists?(node[:passenger][:root_path]) end
-  end
+template "#{node[:apache][:dir]}/mods-available/passenger.load" do
+  cookbook "passenger"
+  source "apache.load.erb"
+  owner "root"
+  group "root"
+  mode 0644
+  only_if do ::File.exists?(node[:passenger][:root_path]) end
 end
 
 template "#{node[:apache][:dir]}/mods-available/passenger.conf" do
