a1f8308f03117ed3fe346aee42da52c5757266fd
David Yip <yipdw@northwestern.edu>
2012-05-24 18:58:17 -0500

    Aker cookbook.

---
+++ chef/cookbooks/aker/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Northwestern University"
+maintainer_email "sysadmin@bioinformatics.northwestern.edu"
+license          "Apache 2.0"
+description      "Installs/Configures aker"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "1.0"
