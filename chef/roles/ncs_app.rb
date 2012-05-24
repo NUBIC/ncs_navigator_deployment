26ea8f3d84c4fa498df73a0f5935e899a1bd8b61
David Yip <yipdw@member.fsf.org>
2012-05-24 02:15:57 -0500

    Install RVM in the app server.

--- chef/roles/ncs_app.rb
+++ chef/roles/ncs_app.rb
@@ -5,12 +5,18 @@ run_list(
   "role[ncs_common]",
   "recipe[build-essential]",
   "recipe[postgresql::client]",
-  "recipe[rvm::ruby_193]",
+  "recipe[rvm::system]",
   "recipe[passenger::apache2]"
 )
 
 default_attributes(
   "postgresql" => {
     "version" => "9.0"
+  },
+  "rvm" => {
+    "default_ruby" => "1.9.3-p125",
+    "global_gems" => [
+        { "name" => "bundler" }
+     ]
   }
 )
