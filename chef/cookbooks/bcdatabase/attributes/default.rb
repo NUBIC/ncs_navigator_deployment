dce732ea8eafbfeb8d0680c8bed8f50eb1688149
David Yip <yipdw@member.fsf.org>
2012-05-30 01:45:38 -0500

    The bcdatabase cookbook needs the application_users cookbook, too.

--- chef/cookbooks/bcdatabase/attributes/default.rb
+++ chef/cookbooks/bcdatabase/attributes/default.rb
@@ -1,3 +1,5 @@
+include_attribute "application_users"
+
 default[:bcdatabase][:app_group] = node[:application_users][:group] || "app"
 default[:bcdatabase][:directory] = "/etc/nubic/db"
 default[:bcdatabase][:groups] = {}
