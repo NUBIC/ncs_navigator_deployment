dffcd066edbb932862f2c33a2f8f4a2e56bf615a
David Yip <yipdw@northwestern.edu>
2012-05-30 11:48:18 -0500

    Restore postgresql cookbook customizations.

--- chef/cookbooks/postgresql/attributes/default.rb
+++ chef/cookbooks/postgresql/attributes/default.rb
@@ -70,3 +70,5 @@ else
   default[:postgresql][:version] = "8.4"
   set[:postgresql][:dir]         = "/etc/postgresql/#{node[:postgresql][:version]}/main"
 end
+
+default[:postgresql][:interfaces] = ['localhost']
