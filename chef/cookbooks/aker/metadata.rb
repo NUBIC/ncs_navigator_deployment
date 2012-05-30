532570e1ece956789b800286fb1a2f35f257fbd8
David Yip <yipdw@member.fsf.org>
2012-05-30 01:51:09 -0500

    Update metadata for aker and bcdatabase cookbooks.

--- chef/cookbooks/aker/metadata.rb
+++ chef/cookbooks/aker/metadata.rb
@@ -4,3 +4,4 @@ license          "Apache 2.0"
 description      "Installs/Configures aker"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
 version          "1.0"
+depends          "application_users"
