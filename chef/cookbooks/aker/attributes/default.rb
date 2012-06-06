58c39451a91b8e46c4ab728633238c618bd47908
David Yip <yipdw@northwestern.edu>
2012-06-06 17:08:41 -0500

    Conditionally generate NetID/Pers entries in aker::central.

--- chef/cookbooks/aker/attributes/default.rb
+++ chef/cookbooks/aker/attributes/default.rb
@@ -2,10 +2,9 @@ include_attribute 'application_users'
 
 default[:aker][:central][:path] = "/etc/nubic/bcsec-prod.yml"
 default[:aker][:central][:group] = node[:application_users][:group] || 'app'
-default[:aker][:central][:netid][:user] = ''
-default[:aker][:central][:netid][:password] = ''
 default[:aker][:central][:cas][:base_url] = node[:cas][:base_url]
 default[:aker][:central][:cas][:proxy_retrieval_url] = node[:cas][:proxy_retrieval_url]
 default[:aker][:central][:cas][:proxy_callback_url] = node[:cas][:proxy_callback_url]
 default[:aker][:central][:pers][:entry] = node[:pers][:bcdatabase][:entry]
 default[:aker][:central][:pers][:group] = node[:pers][:bcdatabase][:group]
+default[:aker][:central][:netid] = {}
