7279f60d30c80ac35a12de3b87ce637d26fcda4b
David Yip <yipdw@northwestern.edu>
2012-06-08 10:28:12 -0500

    Remove unused ssl_certificates_development LWRP.

--- chef/cookbooks/ssl_certificates/resources/development.rb
+++
@@ -1,6 +0,0 @@
-actions :install, :uninstall
-
-attribute :path, :name_attribute => true
-attribute :basename, :kind_of => String, :required => true
-attribute :owner, :kind_of => String, :required => true
-attribute :group, :kind_of => String, :required => true
