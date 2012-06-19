6d4f571f37409399198078aa6855dc552d1dd6fc
David Yip <yipdw@northwestern.edu>
2012-06-18 23:36:18 -0500

    Bump logrotate version to 0.9.0.

--- chef/cookbooks/logrotate/metadata.rb
+++ chef/cookbooks/logrotate/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Keeps logrotate package updated and has definition for logrotate configs"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.8.2"
+version           "0.9.0"
 
 recipe "logrotate", "Keeps logrotate package updated"
 
