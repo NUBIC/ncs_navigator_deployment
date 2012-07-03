600deba35597447d4ce8b0d8f0e4843430c1e2b6
David Yip <yipdw@northwestern.edu>
2012-07-03 13:28:12 -0500

    Don't issue a restart notification when no properties have changed.
    Also use |= instead of += to eliminate duplicate property entries.

--- chef/cookbooks/tomcat/definitions/tomcat_property.rb
+++ chef/cookbooks/tomcat/definitions/tomcat_property.rb
@@ -5,8 +5,12 @@ define :tomcat_property, :action => :enable do
 
   ruby_block "Tomcat properties: #{properties.join(', ')}" do
     block do
-      node["tomcat"]["custom_properties"] += properties
+      node["tomcat"]["custom_properties"] |= properties
       node.save unless Chef::Config[:solo]
+    end
+
+    not_if do
+      properties.all? { |p| node["tomcat"]["custom_properties"].include?(p) }
     end
 
     notifies :create, resources(:template => node["tomcat"]["properties_file"])
