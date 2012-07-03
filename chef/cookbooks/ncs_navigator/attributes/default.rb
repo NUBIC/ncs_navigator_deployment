b9a5c414deaa5f49f77c764c865a3021c4f71cca
David Yip <yipdw@northwestern.edu>
2012-07-03 15:16:45 -0500

    Remove obsolete attribute.

--- chef/cookbooks/ncs_navigator/attributes/default.rb
+++ chef/cookbooks/ncs_navigator/attributes/default.rb
@@ -36,7 +36,6 @@ default[:ncs_navigator][:env] = "production"
 
 default[:ncs_navigator][:authority][:psc][:ca_file] = ""
 
-default[:ncs_navigator][:study_center][:sampling_units_file] = "/etc/nubic/ncs/ssu.csv"
 default[:ncs_navigator][:study_center][:username] = "NetID"
 default[:ncs_navigator][:study_center][:footer_logo_left][:checksum] = "2d288345f95e90c25a59068b6eab776c82ff6c555a80d41322ba203bccc9d524"
 default[:ncs_navigator][:study_center][:footer_logo_left][:path] = "/var/www/apps/ncs_shared/footer_logo_left.png"
