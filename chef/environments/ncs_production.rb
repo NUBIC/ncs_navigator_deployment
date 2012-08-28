4b8b0daa92c73065680d550c29c4daa7b18728d9
David Yip <yipdw@northwestern.edu>
2012-08-28 15:52:31 -0500

    Don't use the NCS machine account changes just yet.

--- chef/environments/ncs_production.rb
+++ chef/environments/ncs_production.rb
@@ -1,2 +1,7 @@
 name "ncs_production"
 description "Production environment"
+
+cookbook_versions(
+  "cas" => "< 1.0.0",
+  "ncs_navigator" => "~> 0.0.0"
+)
--- chef/environments/ncs_staging.rb
+++ chef/environments/ncs_staging.rb
@@ -1,2 +1,7 @@
 name "ncs_staging"
 description "Staging environment"
+
+cookbook_versions(
+  "cas" => "< 1.0.0",
+  "ncs_navigator" => "~> 0.0.0"
+)
