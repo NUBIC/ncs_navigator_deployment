3cca136ba0282eb7a112d7b9654ed926fe3d4a7c
David Yip <yipdw@northwestern.edu>
2012-09-05 12:36:48 -0500

    Build CA bundle.

--- chef/cookbooks/ssl_certificates/attributes/default.rb
+++ chef/cookbooks/ssl_certificates/attributes/default.rb
@@ -1,15 +1,9 @@
 default[:ssl_certificates][:ca_path] = "/etc/pki/tls/certs"
 default[:ssl_certificates][:key_path] = "/etc/pki/tls/private"
-
-# rapidssl.crt
-# SHA1 Fingerprint=C0:39:A3:26:9E:E4:B8:E8:2D:00:C5:3F:A7:97:B5:A1:9E:83:6F:47
-# MD5 Fingerprint=1B:EE:28:5E:8F:F8:08:5F:79:CC:60:8B:92:99:A4:53
-
-# geotrust.crt
-# SHA1 Fingerprint=73:59:75:5C:6D:F9:A0:AB:C3:06:0B:CE:36:95:64:C8:EC:45:42:A3
-# MD5 Fingerprint=2E:7D:B2:A3:1D:0E:3D:A4:B2:5F:49:B9:54:2A:2E:1A
+default[:ssl_certificates][:ca_bundle] = node[:ssl_certificates][:ca_path] + "/ca_bundle.crt"
 
 set[:ssl_certificates][:trust_chain] = %W(
+  Equifax_Secure_Certificate_Authority.pem
+  GeoTrust_Global_CA.pem
   rapidssl.crt
-  geotrust.crt
 )
--- chef/cookbooks/ssl_certificates/recipes/default.rb
+++ chef/cookbooks/ssl_certificates/recipes/default.rb
@@ -3,19 +3,32 @@
 # Recipe:: default
 #
 # Copyright 2011, Northwestern University
-
 ca_path = node[:ssl_certificates][:ca_path]
 trust_chain = node[:ssl_certificates][:trust_chain]
 
 paths = trust_chain.map { |c| "#{ca_path}/#{c}" }
 
 paths.each do |cert_path|
+  # Install each certificate for later use by e.g. Java keystore recipes...
   cookbook_file cert_path do
     action :create
     mode 0444
   end
 
+  # ...install lookup links for OpenSSL...
   ssl_certificates cert_path do
     action :trust
   end
+end
+
+# ...and then build the bundle.
+#
+# We need both of these to occur because we want to present a certificate chain
+# to HTTP clients, but we want to use the same certificates in Java trust
+# stores and keytool will not read multiple certificates out of a single file.
+bash "build CA bundle" do
+  code <<-END
+  umask 0333
+  cat #{paths.join(' ')} > #{node[:ssl_certificates][:ca_bundle]}
+  END
 end
