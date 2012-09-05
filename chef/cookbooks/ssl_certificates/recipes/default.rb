f277b142aea07b4f5006043da786631ac3cb63a3
David Yip <yipdw@northwestern.edu>
2012-09-05 13:09:32 -0500

    Only regenerate the CA bundle if any constituents change.

--- chef/cookbooks/ssl_certificates/recipes/default.rb
+++ chef/cookbooks/ssl_certificates/recipes/default.rb
@@ -8,27 +8,46 @@ trust_chain = node[:ssl_certificates][:trust_chain]
 
 paths = trust_chain.map { |c| "#{ca_path}/#{c}" }
 
+# Build the bundle.
+#
+# We need both the bundle and individual certificates because we want to
+# present a certificate chain to HTTP clients, but we want to use the same
+# certificates in Java trust stores and keytool will not read multiple
+# certificates out of a single file.
+#
+# This is only run if any of the constituent certificates change or the bundle
+# is missing.
+script "build_trust_chain_bundle" do
+  action :nothing
+  code <<-END
+  BUNDLE='#{node[:ssl_certificates][:trust_chain_bundle]}'
+
+  for path in #{paths.join(' ')}; do
+    cat $path >> $BUNDLE
+    echo >> $BUNDLE
+  done
+  END
+  interpreter "bash"
+end
+
 paths.each do |cert_path|
   # Install each certificate for later use by e.g. Java keystore recipes...
   cookbook_file cert_path do
     action :create
     mode 0444
+    notifies :execute, resource(:script => "build_trust_chain_bundle]")
   end
 
-  # ...install lookup links for OpenSSL...
+  # ...and install lookup links for OpenSSL.
   ssl_certificates cert_path do
     action :trust
   end
 end
 
-# ...and then build the bundle.
-#
-# We need both of these to occur because we want to present a certificate chain
-# to HTTP clients, but we want to use the same certificates in Java trust
-# stores and keytool will not read multiple certificates out of a single file.
-bash "build trust chain bundle" do
-  code <<-END
-  umask 0333
-  cat #{paths.join(' ')} > #{node[:ssl_certificates][:trust_chain_bundle]}
-  END
+# Build the bundle if necessary.
+file node[:ssl_certificates][:trust_chain_bundle] do
+  action :create_if_missing
+  mode 0444
+
+  notifies :execute, resource(:script => "build_trust_chain_bundle]")
 end
