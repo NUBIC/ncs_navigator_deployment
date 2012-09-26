0c29fb52226d7b25d87fc773961159f4848946a9
David Yip <yipdw@northwestern.edu>
2012-09-26 12:26:46 -0500

    We need to mind SELinux file contexts in zeroconf now.  #2413.
    The zeroconf recipe is now run before the recipe that disables SELinux
    enforcement.  For CentOS base boxes, this means that SELinux will be
    enabled.

--- chef/cookbooks/zeroconf/recipes/default.rb
+++ chef/cookbooks/zeroconf/recipes/default.rb
@@ -31,10 +31,30 @@ end
 
 iptables_rule "zeroconf_in", :cookbook => "zeroconf"
 
+# The zeroconf recipe may be run as part of preparing a
+# straight-from-the-vendor image, which (in the case of CentOS, at least) means
+# that we need to deal with SELinux security contexts.  (There's usually a
+# recipe to turn SELinux off, but it'll run after this one.)
+bash "restore_avahi_conf_security_context" do
+  action :nothing
+  code <<-END
+    if [ `which restorecon` ] && [ `which selinuxenabled` ]; then
+      `selinuxenabled`
+      SELINUXENABLED=$?
+
+      if [ $SELINUXENABLED -eq 0 ]; then
+        restorecon "/etc/avahi/avahi-daemon.conf"
+      fi
+    fi
+  END
+end
+
 template "/etc/avahi/avahi-daemon.conf" do
   source "avahi-daemon.conf.erb"
   mode 0444
   variables(:allowed_interfaces => node[:zeroconf][:allowed_interfaces])
+
+  notifies :run, resources(:bash => "restore_avahi_conf_security_context"), :immediately
 end
 
 ruby_block "rebuild iptables" do
