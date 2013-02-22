4cef7ee384bf2d1b4065608398991db23dd55a03
David Yip <yipdw@northwestern.edu>
2013-02-22 11:49:38 -0600

    Bump PostgreSQL in development to 9.1.8-2PGDG, too.

--- chef/environments/ncs_development.rb
+++ chef/environments/ncs_development.rb
@@ -4,6 +4,6 @@ description "Development environment"
 default_attributes(
   :development => true,
   'postgresql' => {
-    'package_version' => '9.1.8-1PGDG.rhel6'
+    'package_version' => '9.1.8-2PGDG.rhel6'
   }
 )
