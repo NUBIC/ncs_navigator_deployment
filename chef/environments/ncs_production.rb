9d9575c9e80ce8c9819e58aa6ddbc6322143365b
David Yip <yipdw@northwestern.edu>
2013-02-27 13:22:02 -0600

    We no longer use these environments.

--- chef/environments/ncs_production.rb
+++
@@ -1,8 +0,0 @@
-name "ncs_production"
-description "Production environment"
-
-default_attributes(
-  'postgresql' => {
-    'package_version' => '9.1.6-1PGDG.rhel6'
-  }
-)
--- chef/environments/ncs_staging.rb
+++
@@ -1,8 +0,0 @@
-name "ncs_staging"
-description "Staging environment"
-
-default_attributes(
-  'postgresql' => {
-    'package_version' => '9.1.8-2PGDG.rhel6'
-  }
-)
