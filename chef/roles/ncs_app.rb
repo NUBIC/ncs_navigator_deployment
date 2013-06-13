e448a28322c42a845a2c79bdfecc8c9f9becd675
David Yip <yipdw@northwestern.edu>
2013-06-13 17:17:31 -0500

    Add Cases and Ops deploy monitoring to study-center-based nodes.

--- chef/roles/ncs_app.rb
+++ chef/roles/ncs_app.rb
@@ -54,5 +54,13 @@ default_attributes(
   },
   "bcdatabase" => {
     "version" => "1.2.1"
+  },
+  "ncs_navigator_nubic" => {
+    "deploy_monitor" => {
+      "monitor" => {
+        "cases" => true,
+        "ops" => true
+      }
+    }
   }
 )
