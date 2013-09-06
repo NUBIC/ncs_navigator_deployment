e75f6fc74b887bb55afee35d3ad8177565a37c72
David Yip <yipdw@northwestern.edu>
2013-09-06 15:31:22 -0500

    Fix typo.

--- chef/cookbooks/ncs_navigator/metadata.rb
+++ chef/cookbooks/ncs_navigator/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "YOUR_EMAIL"
 license          "Apache 2.0"
 description      "Installs/Configures ncs_navigator"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.3.141592653589793238462643383279502884197169'
+version          "0.3.141592653589793238462643383279502884197169"
 
 depends "aker"
 depends "apache2"
