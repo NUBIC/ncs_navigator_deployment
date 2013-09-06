a085d88a9ed776862b17eae29c255547db4686df
David Yip <yipdw@northwestern.edu>
2013-09-06 15:26:59 -0500

    Lock production to pre-shutdown cookbook.
    For now.

--- chef/environments/ncs_production.rb
+++ chef/environments/ncs_production.rb
@@ -1,6 +1,10 @@
 name "ncs_production"
 description "Production environment"
 
+cookbook_versions(
+  'ncs_navigator' => '0.2.0'
+)
+
 default_attributes(
   'postgresql' => {
     'package_version' => '9.1.6-1PGDG.rhel6'
--- chef/environments/ncs_staging.rb
+++ chef/environments/ncs_staging.rb
@@ -1,6 +1,10 @@
 name "ncs_staging"
 description "Staging environment"
 
+cookbook_versions(
+  'ncs_navigator' => '~> 0.3.0'
+)
+
 default_attributes(
   'postgresql' => {
     'package_version' => '9.1.8-2PGDG.rhel6'
